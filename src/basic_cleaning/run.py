535e4e7438304c48c23eb6f27ea96a26976870a3
Winfield-T <64602123+Winfield-T@users.noreply.github.com>
2026-05-17 16:40:03 -0700

    Remove geographic boundary filter from basic_cleaning

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -30,9 +30,6 @@ def go(args):
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
 
-    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
-    df = df[idx].copy()
-
     # Save the cleaned data
     df.to_csv('clean_sample.csv',index=False)
 
